97a97620ed0a9a930337633bf63b6b6ad88e6ce9
True-Kim <53033104+True-Kim@users.noreply.github.com>
2021-05-18 03:36:07 +0900

    pyQt ui

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QProgressBar
+from PyQt5.QtCore import Qt
 
 # 창 띄우기
-class loginWidget(QWidget):
+class mainWidget(QWidget):
     def __init__(self, parent=None, ticker="BTC"):
         super().__init__(parent)
-        uic.loadUi("source/login.ui", self)
+        uic.loadUi("source/main.ui", self)
         self.ticker = ticker
 
 if __name__ == "__main__":
     import sys
     from PyQt5.QtWidgets import QApplication
     app = QApplication(sys.argv)
-    ow = loginWidget()
+    ow = mainWidget()
     ow.show()
     exit(app.exec_())
+
+    
